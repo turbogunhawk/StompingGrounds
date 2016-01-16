5dda96a3905291e204bf0853e4316e903a9c2951
Ted Landis <landis.edward@gmail.com>
2016-01-16 12:54:36 -0500

    uh we restart again

--- app/src/main/java/me/tedlandis/stompinggrounds/MvpView.java
+++
@@ -1,7 +0,0 @@
-package me.tedlandis.stompinggrounds;
-
-/**
- * root of mvp views
- */
-public interface MvpView {
-}
---
+++ app/src/main/java/me/tedlandis/stompinggrounds/StompingGroundsApplication.java
@@ -0,0 +1,13 @@
+package me.tedlandis.stompinggrounds;
+
+import android.app.Application;
+
+/**
+ * WRITE SOMETHING HERE!!!
+ */
+public class StompingGroundsApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
